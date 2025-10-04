b732ba9c7f20da7872cf55e354d99b21acb90c8b
samuel <samuelzenebe7@gmail.com>
2025-10-04 09:29:34 +0300

    update-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+export default async function Home() {
+  type PostType = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  };
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) {
+    return <h1> Somehow failed. </h1>;
+  }
+  const postsJson: PostType[] = await response.json();
 
-export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-         <h1> {'this is next js app on testing.'.toUpperCase()} </h1>
+      <h1 className="text-2xl text-center"> Mock Posts </h1>
+      <div className="border border-amber-950 flex flex-col gap-2 items-center justify-start">
+        {postsJson.map((post: PostType) => (
+          <div className="shadow-lg p-2" key={post.id}>
+            <h2 className="text-left text-amber-900 font-semibold p-1">
+              {post.title}
+            </h2>
+            <p className="prose text-muted">{post.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
